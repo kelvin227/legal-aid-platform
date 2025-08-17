a12ca64d1d456f2dd9f958b9f694182267aa9184
kelvin ngene <162456111+kelvin227@users.noreply.github.com>
2025-08-17 22:46:40 +0100

    cha

--- app/web/signin/page.tsx
+++ app/web/signin/page.tsx
@@ -66,7 +66,6 @@ export default function LawyerLoginPage() {
 
       if (response.success) {
         toast.success(response.message || "Login successful!");
-        router.replace("/dashboard"); // Redirect to dashboard on success
       } else {
         setError(
           response.message || "Login failed. Please check your credentials."
